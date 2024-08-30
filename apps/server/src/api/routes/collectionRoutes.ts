3e10e58f5c94ebd644db4aaf1e0b57b1354578fd
Herbert Tamayo <hftamayo@gmail.com>
2024-08-30 14:31:39 -0600

    feat: collection entity routes

---
+++ apps/server/src/api/routes/collectionRoutes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import * as collectionController from "../controllers/collectionController";
+
+const router: Router = Router();
+
+router.get("/collections", collectionController.getCollections);
+router.get("/collection/:id", collectionController.getCollectionById);
+router.post("/collection", collectionController.createCollection);
+router.put("/collection/:id", collectionController.updateCollection);
+router.delete("/collection/:id", collectionController.deleteCollection);
+
+export default router;
