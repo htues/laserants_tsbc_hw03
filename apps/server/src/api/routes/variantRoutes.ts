0870d13066bb947fb934fb311f678afe0b64c66b
Herbert Tamayo <hftamayo@gmail.com>
2024-08-30 13:27:54 -0600

    feat: variant entity routes

---
+++ apps/server/src/api/routes/variantRoutes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import * as variantController from "../controllers/variantController";
+
+const router: Router = Router();
+
+router.get("/variants", variantController.getVariants);
+router.get("/variant/:id", variantController.getVariantById);
+router.post("/variant", variantController.createVariant);
+router.put("/variant/:id", variantController.updateVariant);
+router.delete("/variant/:id", variantController.deleteVariant);
+
+export default router;
