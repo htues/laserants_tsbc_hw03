461f99cd8506c635afff3f64df7a3d3e4ee82def
Herbert Tamayo <hftamayo@gmail.com>
2024-08-31 21:59:51 -0600

    feat: seedRoles module

---
+++ apps/server/src/utils/seedRoles.ts
@@ -0,0 +1,36 @@
+import prisma from "../api/prismaClient";
+
+const roles = [
+  {
+    name: "ADMINISTRATOR",
+    description: "Admin users have full access to all resources",
+    status: true,
+  },
+    {
+        name: "SUPERVISOR",
+        description: "supervisor users have access to most resources",
+        status: true,
+    },
+  {
+    name: "USER",
+    description: "User users have limited access to resources",
+    status: true,
+  },
+];
+
+async function seedRoles() {
+    try{
+        for (const role of roles) {
+            await prisma.role.upsert({
+                where: { name: role.name },
+                update: {},
+                create: role,
+            });
+        }
+        console.log("Roles seeded successfully");
+    } catch (error: unknown) {
+        console.error("Failed to seed roles", error);
+    } finally {
+        await prisma.$disconnect();
+    }
+}
