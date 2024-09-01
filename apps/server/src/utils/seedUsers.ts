2b9e1122638adf22a07b628d751d34cfd169368d
Herbert Tamayo <hftamayo@gmail.com>
2024-08-31 22:26:55 -0600

    feat: first part of usersSeed

---
+++ apps/server/src/utils/seedUsers.ts
@@ -0,0 +1,25 @@
+import prisma from "../api/prismaClient";
+
+const users = [
+    {
+        lastname: "Doe",
+        firstname: "John",
+        email: "jdoe@astros.com",
+        password: "password",
+        confirmPassword: "password",
+    },
+    {
+        lastname: "Doe",
+        firstname: "Jane",
+        email: "jane@astros.com",
+        password: "password",
+        confirmPassword: "password",
+    },
+    {
+        lastname: "Doe",
+        firstname: "Jack",
+        email: "jack@astros.com",
+        password: "password",
+        confirmPassword: "password",
+    }
+];
