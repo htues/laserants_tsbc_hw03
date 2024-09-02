f1d520777264be557d49c7ef44e6c26b825d82b5
Herbert Tamayo <hftamayo@gmail.com>
2024-09-02 11:56:50 -0600

    feat: cleaning the method execution on server load

--- apps/server/src/besetup.ts
+++ apps/server/src/besetup.ts
@@ -19,39 +19,37 @@ const backend: express.Application = express()
 const PORT = port || 5001
 
 async function StartBackend() {
+  await dbConnection()
+  backend.use(cors(setCorsEnviro))
+  backend.use(express.json())
+  backend.use(express.urlencoded({ extended: true }))
+  backend.use(cookieParser())
+
+  // Method for request monitoring and logging
+  backend.use((req: Request, res: Response, next: NextFunction) => {
+    console.log(`Request received: [${req.method}] ${req.path}`)
+    console.log(`Request headers: ${JSON.stringify(req.headers)}`)
+    next()
+  })
+
+  await seedDatabase()
+
+  backend.use('/health', healthCheckRouter)
+  backend.use('/roles', rolesRouter)
+  backend.use('/users', usersRouter)
+  backend.use('/categories', categoriesRouter)
+  backend.use('/products', productsRouter)
+  backend.use('/variants', variantsRouter)
+  backend.use('/collections', collectionsRouter)
+
+  // Error handling middleware
+  backend.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error('Unexpected error:', err.message)
+    console.error('Stack trace:', err.stack)
+    res.status(500).send('An unexpected error occurred')
+  })
+
   try {
-    await dbConnection()
-    backend.use(cors(setCorsEnviro))
-    backend.use(express.json())
-    backend.use(express.urlencoded({ extended: true }))
-    backend.use(cookieParser())
-
-    // Method for request monitoring and logging
-    backend.use((req: Request, res: Response, next: NextFunction) => {
-      console.log(`Request received: [${req.method}] ${req.path}`)
-      console.log(`Request headers: ${JSON.stringify(req.headers)}`)
-      next()
-    })
-
-    await seedDatabase()
-
-    backend.use('/health', healthCheckRouter)
-    backend.use('/roles', rolesRouter)
-    backend.use('/users', usersRouter)
-    backend.use('/categories', categoriesRouter)
-    backend.use('/products', productsRouter)
-    backend.use('/variants', variantsRouter)
-    backend.use('/collections', collectionsRouter)
-
-    // Error handling middleware
-    backend.use(
-      (err: Error, req: Request, res: Response, next: NextFunction) => {
-        console.error('Unexpected error:', err.message)
-        console.error('Stack trace:', err.stack)
-        res.status(500).send('An unexpected error occurred')
-      },
-    )
-
     const server = backend.listen(PORT, () => {
       console.log(`Server running in ${mode} mode on port ${PORT}`)
     })
@@ -61,5 +59,12 @@ async function StartBackend() {
     process.exit(1)
   }
 }
+
+process.on(
+  'unhandledRejection',
+  (reason: unknown, promise: Promise<unknown>) => {
+    console.error('Unhandled Rejection at:', promise, 'reason:', reason)
+  },
+)
 
 export { backend, StartBackend }
