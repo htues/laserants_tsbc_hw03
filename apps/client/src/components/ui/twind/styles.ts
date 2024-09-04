1e76aa58432e9083cdef834c0a18f4ab91d67076
Herbert Tamayo <hftamayo@gmail.com>
2024-09-04 12:11:15 -0600

    feat: managing twind styles and defining types

---
+++ apps/client/src/components/ui/twind/styles.ts
@@ -0,0 +1,17 @@
+export const toastStyles = {
+    base: "bg-black text-yellow-500",
+    progress: "bg-blue-600",
+  };
+
+  export const dashboardStyles = {
+    footer_body: "flex justify-between items-center bg-black text-white",
+    footer_links: "mx-3 font-semibold hover:opacity-80 duration-150 hover:text-teal-500 hover:underline",
+    sidebar_ul: "mt-3 text-white font-bold",
+    sidebar_li: "mb-2 rounded hover:shadow hover:bg-blue-500 py-2",
+    sidebar_a: "px-3",
+    sidebar_li_icon: "inline-block w-6 h-6 mr-2 -mt-2",
+    sidebar_header_div: "my-2 mb-4",
+    sidebar_header_h1: "text-2x text-white font-bold",
+    header_actionbar_orderOption: "mb-2 rounded hover:shadow hover:bg-gray-600 hover:text-white py-2",
+    header_actionbar_nav: "bg-gray-800 px-4 py-3 flex justify-between",    
+  };  
---
+++ apps/client/src/types/ui.types.ts
@@ -0,0 +1,4 @@
+export type ActionBarTypes = {
+    sidebarToggle: boolean,
+    setSidebarToggle: React.Dispatch<React.SetStateAction<boolean>>
+}
