c5b3a9726973bf99d00f6f7c32944c0d1104abcc
Herbert Tamayo <hftamayo@gmail.com>
2024-09-06 11:24:37 -0600

    feat: adjusting app component workflow

--- apps/client/src/containers/App.tsx
+++ apps/client/src/containers/App.tsx
@@ -2,19 +2,18 @@ import { useState } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import MainLayout from '../components/ui/layout/MainLayout'
 
 function App() {
-
   return (
     <BrowserRouter>
-    <div 
-    className='text-black bg-cover flex flex-col min-h-screen'>
-      <div>
-        <ToastContainer position='bottom-left' />
-      </div>    
-    </div>
+      <div className="text-black bg-cover flex flex-col min-h-screen">
+        <div>
+          <MainLayout />
+          <ToastContainer position="bottom-left" />
+        </div>
+      </div>
     </BrowserRouter>
-
   )
 }
 
