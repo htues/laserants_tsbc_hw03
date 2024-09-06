a493c545efd39f33e22e173ff67ef429d3b217aa
Herbert Tamayo <hftamayo@gmail.com>
2024-09-06 13:51:05 -0600

    feat: styles and types for product entity

--- apps/client/src/components/ui/twind/styles.ts
+++ apps/client/src/components/ui/twind/styles.ts
@@ -1,24 +1,40 @@
 export const toastStyles = {
-    base: "bg-black text-yellow-500",
-    progress: "bg-blue-600",
-  };
+  base: 'bg-black text-yellow-500',
+  progress: 'bg-blue-600',
+}
 
-  export const dashboardStyles = {
-    container_main_layout: "min-h-screen grid grid-rows-[auto,1fr,auto]",
-    container_main_header: "col-span-2 fixed left-0 right-0 z-10 top-0",
-    container_main_sidebar: "h-full fixed top-[header-height] bottom-[footer-height]",
-    container_main_content: "ml-[sidebar-width] flex items-center justify-center pt-[header-height] pb-[footer-height] min-h-screen",
-    container_main_routes: "w-full max-w-screen-lg px-4",
-    container_main_footer: "col-span-2 fixed left-0 right-0 z-10 bottom-0",
-    footer_body: "flex justify-between items-center bg-black text-white",
-    footer_links: "mx-3 font-semibold hover:opacity-80 duration-150 hover:text-teal-500 hover:underline",
-    sidebar_ul: "mt-3 text-white font-bold",
-    sidebar_li: "mb-2 rounded hover:shadow hover:bg-blue-900 py-2",
-    sidebar_li_active: "font-semibold text-teal-500",
-    sidebar_a: "px-3",
-    sidebar_li_icon: "inline-block w-6 h-6 mr-2 -mt-2",
-    sidebar_header_div: "my-2 mb-4",
-    sidebar_header_h1: "text-2x text-white font-bold",
-    header_actionbar_orderOption: "mb-2 rounded hover:shadow hover:bg-gray-600 hover:text-white py-2",
-    header_actionbar_nav: "bg-gray-800 px-4 py-3 flex justify-between",    
-  };  
+export const dashboardStyles = {
+  container_main_layout: 'min-h-screen grid grid-rows-[auto,1fr,auto]',
+  container_main_header: 'col-span-2 fixed left-0 right-0 z-10 top-0',
+  container_main_sidebar:
+    'h-full fixed top-[header-height] bottom-[footer-height]',
+  container_main_content:
+    'ml-[sidebar-width] flex items-center justify-center pt-[header-height] pb-[footer-height] min-h-screen',
+  container_main_routes: 'w-full max-w-screen-lg px-4',
+  container_main_footer: 'col-span-2 fixed left-0 right-0 z-10 bottom-0',
+  footer_body: 'flex justify-between items-center bg-black text-white',
+  footer_links:
+    'mx-3 font-semibold hover:opacity-80 duration-150 hover:text-teal-500 hover:underline',
+  sidebar_ul: 'mt-3 text-white font-bold',
+  sidebar_li: 'mb-2 rounded hover:shadow hover:bg-blue-900 py-2',
+  sidebar_li_active: 'font-semibold text-teal-500',
+  sidebar_a: 'px-3',
+  sidebar_li_icon: 'inline-block w-6 h-6 mr-2 -mt-2',
+  sidebar_header_div: 'my-2 mb-4',
+  sidebar_header_h1: 'text-2x text-white font-bold',
+  header_actionbar_orderOption:
+    'mb-2 rounded hover:shadow hover:bg-gray-600 hover:text-white py-2',
+  header_actionbar_nav: 'bg-gray-800 px-4 py-3 flex justify-between',
+}
+
+export const productStyles = {
+  dboard_card: 'border rounded-lg shadow-lg overflow-hidden',
+  dboard_image: 'w-full h-48 object-cover',
+  dboard_details: 'p-4',
+  dboard_name: 'text-xl font-bold',
+  dboard_description: 'text-gray-700',
+  dboard_footer: 'flex justify-between items-center mt-4',
+  dboard_price: 'text-lg font-semibold text-green-600',
+  dboard_button:
+    'bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 flex items-center',
+}
---
+++ apps/client/src/types/product.types.ts
@@ -0,0 +1,11 @@
+type Product = {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    imageUrl: string;
+  }
+  
+  export type ProductCardTypes = {
+    product: Product;
+  }
