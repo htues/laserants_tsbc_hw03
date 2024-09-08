fd314c019678f9a9e7ed34902329481ed5b1c769
Herbert Tamayo <hftamayo@gmail.com>
2024-09-08 13:37:06 -0600

    feat: decoupling ui from bl in product details

--- apps/client/src/components/dashboard/products/ProductDetails.tsx
+++ apps/client/src/components/dashboard/products/ProductDetails.tsx
@@ -1,47 +1,28 @@
-import { useEffect, useRef } from 'react'
-import { productStyles } from '../../ui/twind/styles'
+import Modal from '../../ui/forms/Modal'
 import { ProductCardTypes } from '../../../types/product.types'
+import { formStyles } from '../../ui/twind/styles'
 
 const ProductDetails: React.FC<{
   product: ProductCardTypes['product']
   onClose: () => void
 }> = ({ product, onClose }) => {
   const { name, price, description, imageUrl } = product
-  const modalRef = useRef<HTMLDivElement>(null)
-
-  useEffect(() => {
-    const handleKeyDown = (e: KeyboardEvent) => {
-      if (e.key === 'Escape') {
-        onClose()
-      }
-    }
-
-    const handleClickOutside = (e: MouseEvent) => {
-      if (modalRef.current && !modalRef.current.contains(e.target as Node)) {
-        onClose()
-      }
-    }
-
-    document.addEventListener('keydown', handleKeyDown)
-    document.addEventListener('mousedown', handleClickOutside)
-    return () => {
-      document.removeEventListener('keydown', handleKeyDown)
-      document.removeEventListener('mousedown', handleClickOutside)
-    }
-  }, [onClose])
 
   return (
-    <div className={productStyles.modal}>
-      <div className={productStyles.modal_content}>
-        <span className={productStyles.modal_close} onClick={onClose}>
-          &times;
-        </span>
-        <img src={imageUrl} alt={name} className={productStyles.modal_image} />
-        <h2 className={productStyles.modal_name}>{name}</h2>
-        <p className={productStyles.modal_description}>{description}</p>
-        <span className={productStyles.modal_price}>${price.toFixed(2)}</span>
-      </div>
-    </div>
+    <Modal onClose={onClose}>
+      <img
+        src={imageUrl}
+        alt={name}
+        className={formStyles.modal_productdetails_image}
+      />
+      <h2 className={formStyles.modal_productdetails_name}>{name}</h2>
+      <p className={formStyles.modal_productdetails_description}>
+        {description}
+      </p>
+      <span className={formStyles.modal_productdetails_price}>
+        ${price.toFixed(2)}
+      </span>
+    </Modal>
   )
 }
 
