55edc62219c18298afdfbd9108dbc26c2c6c328d
Herbert Tamayo <hftamayo@gmail.com>
2024-09-10 14:41:19 -0600

    feat: refactoring loadData and Categories to fetch correctly the data

--- apps/client/src/api/loadData.ts
+++ apps/client/src/api/loadData.ts
@@ -1,47 +1,45 @@
-import { useDispatch } from 'react-redux'
-import { getCategories } from '../components/redux/categorySlice'
-import { AppDispatch } from '../components/redux/store'
-import { toast } from 'react-toastify'
-import { unwrapResult } from '@reduxjs/toolkit'
+import { AppDispatch } from '../components/redux/store';
+import { unwrapResult } from '@reduxjs/toolkit';
+import { toast } from 'react-toastify';
+import { getCategories } from '../components/redux/categorySlice';
 
 // type fetchAction = typeof getCategories | typeof getProducts
-type fetchAction = typeof getCategories
+type FetchAction = typeof getCategories;
 
 const loadData = async (
   tries: number,
   setTries: (tries: number | ((prevTries: number) => number)) => void,
-  actionToDispatch: fetchAction,
+  actionToDispatch: FetchAction,
+  dispatch: AppDispatch
 ) => {
-  const dispatch = useDispatch<AppDispatch>()
-
   if (tries > 3) {
-    toast.error('Failed to load data')
-    return
+    toast.error('Failed to load data');
+    return;
   }
 
   try {
-    const action = await dispatch<any>(actionToDispatch())
-    unwrapResult(action)
+    const action = await dispatch<any>(actionToDispatch());
+    unwrapResult(action);
 
     if (actionToDispatch.fulfilled.match(action)) {
-      console.log('categories status: success')
-      setTries(0)
+      console.log('categories status: success');
+      setTries(0);
     } else if (actionToDispatch.rejected.match(action)) {
-      console.log('categories status: failed')
+      console.log('categories status: failed');
       setTries((prevTries: number) => {
         if (prevTries < 3) {
-          loadData(prevTries + 1, setTries, actionToDispatch)
-          return prevTries + 1
+          loadData(prevTries + 1, setTries, actionToDispatch, dispatch);
+          return prevTries + 1;
         } else {
-          console.log('failed to load after 3 attempts')
-          return prevTries
+          console.log('failed to load after 3 attempts');
+          return prevTries;
         }
-      })
+      });
     }
   } catch (error) {
-    console.error('Error loading data:', error)
-    toast.error('An error occurred while loading data')
+    console.error('Error loading data:', error);
+    toast.error('An error occurred while loading data');
   }
-}
+};
 
-export default loadData
+export default loadData;
--- apps/client/src/components/dashboard/sidebar/Categories.tsx
+++ apps/client/src/components/dashboard/sidebar/Categories.tsx
@@ -1,23 +1,26 @@
-import { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import loadData from '../../../api/loadData'
-import { getCategories } from '../../redux/categorySlice'
-import { selectCategories } from '../../redux/categorySelector'
-import { FaHome, FaRegFileAlt } from 'react-icons/fa'
-import { dashboardStyles } from '../../ui/twind/styles'
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import loadData from '../../../api/loadData';
+import { getCategories } from '../../redux/categorySlice';
+import { selectCategories } from '../../redux/categorySelector';
+import { FaHome, FaRegFileAlt } from 'react-icons/fa';
+import { dashboardStyles } from '../../ui/twind/styles';
 
 function Categories() {
-  const [tries, setTries] = useState(0)
-  const dispatch = useDispatch()
-  const categories = useSelector(selectCategories)
+  const [tries, setTries] = useState(0);
+  const dispatch = useDispatch();
+  const categories = useSelector(selectCategories);
 
   useEffect(() => {
     const fetchCategories = async () => {
-      await loadData(tries, setTries, getCategories)
-      console.log('categories:', categories)
-    }
-    fetchCategories()
-  }, [tries, dispatch, categories])
+      await loadData(tries, setTries, getCategories, dispatch);
+    };
+    fetchCategories();
+  }, [tries, dispatch]);
+
+  useEffect(() => {
+    console.log('Fetched categories:', categories);
+  }, [categories]);
 
   return (
     <span>
@@ -28,11 +31,8 @@ function Categories() {
             All Products
           </a>
         </li>
-      </ul>
-
-      {categories.map((category) => (
-        <ul key={category.id} className={dashboardStyles.sidebar_ul}>
-          <li className={dashboardStyles.sidebar_li}>
+        {categories.map((category) => (
+          <li key={category.id} className={dashboardStyles.sidebar_li}>
             <a
               href={`/categories/${category.id}`}
               className={dashboardStyles.sidebar_a}
@@ -41,10 +41,10 @@ function Categories() {
               {category.name}
             </a>
           </li>
-        </ul>
-      ))}
+        ))}
+      </ul>
     </span>
-  )
+  );
 }
 
-export default Categories
+export default Categories;
