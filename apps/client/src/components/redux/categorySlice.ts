13dd5b4ecbcfdc102c3feac77f6d4267d2f1f1cd
Herbert Tamayo <hftamayo@gmail.com>
2024-09-09 20:37:24 -0600

    feat: categorySlice with bugs

--- apps/client/src/components/redux/categorySlice.ts
+++ apps/client/src/components/redux/categorySlice.ts
@@ -9,13 +9,53 @@ import {
 import { RootState } from './rootSlice'
 
 export const getCategories = createAsyncThunk(
-    'categories/getCategories',
-    async (_, { rejectWithValue }) => {
-        try {
-        const response: CategoryResponse = await productsOps.getCategories()
-        return response.categories?.reverse()
-        } catch (error) {
-        return rejectWithValue((error as ApiError).response.data.message)
-        }
-    },
+  'categories/getCategories',
+  async (_, { rejectWithValue }) => {
+    try {
+      const response: CategoryResponse = await productsOps.getCategories()
+      return response.categories?.reverse()
+    } catch (error) {
+      const apiError: ApiError = error as ApiError
+      return rejectWithValue(
+        apiError.response
+          ? apiError.response.data.message
+          : 'An unknown error occurred',
+      )
+    }
+  },
 )
+
+const initialState: CategoryState = {
+  categories: [],
+  status: 'idle',
+  error: null,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getCategories.pending, (state) => {
+      state.loading = true
+      state.status = 'loading'
+      state.error = null
+  });
+
+  builder.addCase(getCategories.fulfilled, (state, action) => {
+    state.loading = false
+    state.status = 'fulfilled'
+    state.categories = (
+      action.payload as Category[]
+    ).map((category) => {
+      return {
+        ...category,
+        products: [],
+      }
+    });
+    )
+  });
+
+  builder.addCase(getCategories.rejected, (state, action) => {
+    state.loading = false
+    state.status = 'rejected'
+    state.error = action.error.message as string
+  });
+}
+
+export default categorySlice.reducer
