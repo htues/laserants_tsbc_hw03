247f9f15d6b605d5c8625c06d41d5db374705697
Herbert Tamayo <hftamayo@gmail.com>
2024-09-13 11:14:48 -0600

    feat: adding vite extra config to read centralized env file

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+dotenv.config({ path: path.resolve(__dirname, '../../.env') })
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': process.env,
+  },
 })
