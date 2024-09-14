22c7435e824154dee31adeffc4a528d1c83d52ef
Herbert Tamayo <hftamayo@gmail.com>
2024-09-13 20:28:43 -0600

    feat: splitting the process to read and load vars from environment

--- apps/server/src/config/envvars.ts
+++ apps/server/src/config/envvars.ts
@@ -1,36 +1,36 @@
-import dotenv from 'dotenv'
-import path from 'path'
-import { fileURLToPath } from 'url'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
-dotenv.config({ path: path.resolve(__dirname, '../../../../.env') })
 /*
 this is because of the monorepo:
 - to use one single env file I have to interpolate the
 parameter connection string
 - spinning up just the server the env vars are correctly interpolated
+
+console.log('Environment variables loaded:')
+console.log(`POSTGRES_USER: ${process.env.POSTGRES_USER}`)
+console.log(`POSTGRES_PASSWORD: ${process.env.POSTGRES_PASSWORD}`)
+console.log(`POSTGRES_DB: ${process.env.POSTGRES_DB}`)
+console.log(`POSTGRES_HOST: ${process.env.POSTGRES_HOST}`)
+console.log(`POSTGRES_PORT: ${process.env.POSTGRES_PORT}`)
+
 */
 
 const getEnvVar = (key: string, defaultValue: string = ''): string => {
-  const value = process.env[key];
+  const value = process.env[key]
   if (value === undefined) {
-    throw new Error(`Missing required environment variable: ${key}`);
+    throw new Error(`Missing required environment variable: ${key}`)
   }
-  return value;
-};
+  return value
+}
 
 // Gather environment variables
-const POSTGRES_USER = getEnvVar('POSTGRES_USER');
-const POSTGRES_PASSWORD = getEnvVar('POSTGRES_PASSWORD');
-const POSTGRES_DB = getEnvVar('POSTGRES_DB');
-const POSTGRES_HOST = getEnvVar('POSTGRES_HOST');
-const POSTGRES_PORT = getEnvVar('POSTGRES_PORT');
+const POSTGRES_USER = getEnvVar('POSTGRES_USER')
+const POSTGRES_PASSWORD = getEnvVar('POSTGRES_PASSWORD')
+const POSTGRES_DB = getEnvVar('POSTGRES_DB')
+const POSTGRES_HOST = getEnvVar('POSTGRES_HOST')
+const POSTGRES_PORT = getEnvVar('POSTGRES_PORT')
 
 // Manually construct the DATABASE_URL
-const DATABASE_URL = `postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}`;
-process.env.DATABASE_URL = DATABASE_URL;
+const DATABASE_URL = `postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}`
+process.env.DATABASE_URL = DATABASE_URL
 
 const port = parseInt(process.env.BACKEND_PORT ?? '8003')
 if (isNaN(port)) {
---
+++ apps/server/src/config/loadEnv.ts
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+//using an absolute path
+//dotenv.config({ path: path.resolve('/app/.env') })
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const envPath = path.resolve(__dirname, '../../../../.env')
+console.log(`Resolved .env path: ${envPath}`)
+const result = dotenv.config({ path: envPath })
+
+if (result.error) {
+  console.error('Error loading .env file:', result.error)
+} else {
+  console.log('.env file loaded successfully')
+  /*
+  console.log('Environment variables:')
+  console.log(`POSTGRES_USER: ${process.env.POSTGRES_USER}`)
+  console.log(`POSTGRES_PASSWORD: ${process.env.POSTGRES_PASSWORD}`)
+  console.log(`POSTGRES_DB: ${process.env.POSTGRES_DB}`)
+  console.log(`POSTGRES_HOST: ${process.env.POSTGRES_HOST}`)
+  console.log(`POSTGRES_PORT: ${process.env.POSTGRES_PORT}`)
+  */
+}
