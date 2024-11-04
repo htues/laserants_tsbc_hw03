4b5b68b52daafca3e52160029ea62f3b02a3a667
Herbert Tamayo <hftamayo@gmail.com>
2024-11-04 15:57:52 -0600

    fix: including run migration during data seeding workflow

--- apps/server/src/utils/seedDatabase.ts
+++ apps/server/src/utils/seedDatabase.ts
@@ -1,17 +1,34 @@
 import { mode, dataseeddev, data_structure } from '../config/envvars.js'
-import clearDatabase from './clearDatabase.js'
+import runMigrations from './runnerMigrations.js'
 import seedRoles from './seedRoles.js'
 import seedUsers from './seedUsers.js'
 import seedCategories from './seedCategories.js'
 import seedProducts from './seedProducts.js'
+import { isTableExists, clearDatabase } from './dbUtils.js'
 
 async function seedDatabase() {
   try {
     if (dataseeddev) {
+      const rolesTableExists = await isTableExists('Role')
+      const userTableExists = await isTableExists('User')
+      const categoryTableExists = await isTableExists('Category')
+      const productTableExists = await isTableExists('Product')
+
+      if (
+        !rolesTableExists ||
+        !userTableExists ||
+        !categoryTableExists ||
+        !productTableExists
+      ) {
+        console.log('Tables do not exist, running migrations')
+        await runMigrations()
+      }
+
       if (data_structure === 'restart') {
         await clearDatabase()
       }
       console.log(`Seeding database in ${mode} mode`)
+
       await seedRoles()
       await seedUsers()
       await seedCategories()
