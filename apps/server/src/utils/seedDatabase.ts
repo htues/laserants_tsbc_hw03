5a9d82026dc79ffdccb7f0b10abb7228544d02d1
Herbert Tamayo <hftamayo@gmail.com>
2024-11-10 17:57:16 -0600

    fix: removing seeding conditional

--- apps/server/src/utils/seedDatabase.ts
+++ apps/server/src/utils/seedDatabase.ts
@@ -1,4 +1,4 @@
-import { mode, dataseeddev, data_structure } from '../config/envvars.js'
+import { mode, data_structure } from '../config/envvars.js'
 import runMigrations from './runnerMigrations.js'
 import seedRoles from './seedRoles.js'
 import seedUsers from './seedUsers.js'
@@ -8,34 +8,30 @@ import { isTableExists, clearDatabase, dbDisconnect } from './dbUtils.js'
 
 async function seedDatabase() {
   try {
-    if (dataseeddev) {
-      const rolesTableExists = await isTableExists('Role')
-      const userTableExists = await isTableExists('User')
-      const categoryTableExists = await isTableExists('Category')
-      const productTableExists = await isTableExists('Product')
+    const rolesTableExists = await isTableExists('Role')
+    const userTableExists = await isTableExists('User')
+    const categoryTableExists = await isTableExists('Category')
+    const productTableExists = await isTableExists('Product')
 
-      if (
-        !rolesTableExists ||
-        !userTableExists ||
-        !categoryTableExists ||
-        !productTableExists
-      ) {
-        console.log('Tables do not exist, running migrations')
-        await runMigrations()
-      }
-
-      if (data_structure === 'restart') {
-        await clearDatabase()
-      }
-      console.log(`Seeding database in ${mode} mode`)
-
-      await seedRoles()
-      await seedUsers()
-      await seedCategories()
-      await seedProducts()
-    } else {
-      console.log('No seeding required')
+    if (
+      !rolesTableExists ||
+      !userTableExists ||
+      !categoryTableExists ||
+      !productTableExists
+    ) {
+      console.log('Tables do not exist, running migrations')
+      await runMigrations()
     }
+
+    if (data_structure === 'restart') {
+      await clearDatabase()
+    }
+    console.log(`Seeding database in ${mode} mode`)
+
+    await seedRoles()
+    await seedUsers()
+    await seedCategories()
+    await seedProducts()
   } catch (error: unknown) {
     console.error('Error seeding database', error)
   } finally {
